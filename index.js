d9a362c1db2d9a451fa84ac2e3b5fa5d4ec0700b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-22 02:22:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  updateDOM()
 });
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
